6b6a80b1b19bb4841cf2b0c095bb597d9693e972
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-10-11 16:36:15 +0530

    Update script.groovy

--- Jenkins/script.groovy
+++ Jenkins/script.groovy
@@ -12,6 +12,15 @@ def build() {
 
 def deploy() {
   echo "Deploying with ${AZURE_CREDENTIALS}"
+
+  if (BRANCH_NAME == 'main') {
+    echo "Deploying to DEV"
+  } else if (BRANCH_NAME == 'QA') {
+    echo "Deploying to QA"
+  } else {
+    echo "Deploying to PROD"
+  }
+
   echo "Deployed ${params.VERSION} as part of ${params.TYPE} successfully!"
 }
 
