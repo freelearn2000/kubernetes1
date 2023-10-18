396a6f805620748d03b5c75dff188c38a1d702ea
Biju Kumar <63250919+creativehub2000@users.noreply.github.com>
2023-10-18 15:48:50 +0530

    Update Build.bat

--- Jenkins/Build.bat
+++ Jenkins/Build.bat
@@ -1,4 +1,3 @@
 echo "Building the Project : %date% %time%"
 cd ./Frontend
-npm ci && npm run build
-'''xcopy /s /e /i /h /y "./Frontend/build" "./Backend"''';
+npm ci && npm run build && Xcopy .\build ..\Backend\build /E /I /Y
